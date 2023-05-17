53c88f60f2866bb1257db3042b4817ecddf248b1
kenenx <kenen1996@hotmail.co.uk>
2023-05-17 14:07:35 +0100

    add useEffect

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,22 +1,43 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+const colors = {
+  Bladee: '#e7bce7',
+  Ecco2k: '#b1b1b1',
+  ThaiboyDigital: '#ebebeb',
+}
+const random = Math.floor(Math.random() * colors.length)
+console.log(colors)
+console.log(random)
+console.log(colors[random])
 
 export default function DGForm() {
-  const [selectedMember, setSelectedMember] = useState('bladee');
+  const [selectedMember, setSelectedMember] = useState('bladee')
+  const [color, setColor] = useState(colors.Bladee);
+
+
+  useEffect(() => { document.body.style.background = color
+  },[color])
+
   return (
     <>
       <label>
         Pick your favourite Drain Gang member : 
         <select
-          value={selectedMember}
-          onChange={e => setSelectedMember(e.target.value)}
-        >
-          <option value="bladee">Bladee</option>
-          <option value="ecco2k">Ecco2k</option>
-          <option value="thaiboydigital">Thaiboy Digital</option>
+          value={color}
+          onChange={e => setColor(e.target.value)}
+        > {Object.entries(colors).map(([name, value]) =>(
+          <option key={`color--${name}`} value={value}>
+            {name}
+          </option>
+        ))}
         </select>
       </label>
-      <hr />
-      <p>Your favourite Drain Gang member: {selectedMember}</p>
+      {/* <hr />
+      <p>Your favourite Drain Gang member: {selectedMember}</p> */}
     </>
   );
 }
+
+{/* <option value="Bladee">Bladee</option>
+<option value="Ecco2k">Ecco2k</option>
+<option value="Thaiboydigital">Thaiboy Digital</option> */}
